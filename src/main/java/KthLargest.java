71fc72ead0f3395d1962ce59f01c2309dcc4e98c
Nataliia Oliver <psvnatali@gmail.com>
2022-05-22 12:13:17 -0500

    Cleaned code file KthLargest

--- src/main/java/KthLargest.java
+++ src/main/java/KthLargest.java
@@ -1,11 +1,5 @@
 public class KthLargest {
 
-    //Написать алгоритм KthLargest, который принимает на вход массив целых чисел и число k, и возвращает k-тый
-    //максимальный элемент.
-    //Test Data:
-    //({4, 3, 7, 12, 5, 2, 9, 4, 12}, 3) → 9
-    // (12 и 12 - первый и второй самые большие элементы)
-
     public int KthLargestAlgorithm(int[] array, int k) {
         int result = 0;
         int[] sorted = {};
