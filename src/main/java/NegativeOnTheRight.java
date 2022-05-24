1c67fde8215e2c013410cb6d39690e6776b62a59
Nataliia Oliver <psvnatali@gmail.com>
2022-05-23 22:18:26 -0500

    code cleaned

--- src/main/java/NegativeOnTheRight.java
+++ src/main/java/NegativeOnTheRight.java
@@ -1,10 +1,5 @@
 public class NegativeOnTheRight {
 
-    //Написать алгоритм NegativeOnTheRight, который принимает на вход массив целых чисел, и возвращает массив,
-    //в котором все негативные числа находятся во второй половине массива
-    //Test Data:
-    //{4, -3, 7, -12, 5, -2, 9, 4, 12} → {4, 7, 5, 9, 4, 12, -3, -12, -2}
-
     public int[] NegativeOnTheRightAlgorithm(int[] array) {
         int[] result = new int[array.length];
         int count = array.length - 1;
