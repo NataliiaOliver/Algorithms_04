666438d51edb2a9149505d57e34b524401cfefdc
Nataliia Oliver <psvnatali@gmail.com>
2022-05-23 23:40:34 -0500

    code cleaned

--- src/main/java/NumberOccurrences.java
+++ src/main/java/NumberOccurrences.java
@@ -2,12 +2,6 @@ import java.util.Arrays;
 
 public class NumberOccurrences {
 
-    //Написать алгоритм NumberOccurrences,  который принимает на вход массив целых чисел,  и возвращает массив пар
-    //{число, сколько раз число встречается в массиве}
-    // Test Data:
-    //{3, 2, 5, 3, 1, 5, 4, 2, 1, 4, 5, 3, 2, 1, 4, 5, 3, 2, 1} →
-    //{{1, 4}, {2, 4}, {3, 4}, {4, 3}, {5, 4}}
-
     public int[][] NumberOccurrencesAlgorithm(int[] array) {
 
         if (array.length > 0) {
