0b6fb5fb17153022ed558a34a526704f6b87a9eb
Nataliia Oliver <psvnatali@gmail.com>
2022-05-23 21:46:43 -0500

    code cleaned

--- src/main/java/MinMaxAve.java
+++ src/main/java/MinMaxAve.java
@@ -1,11 +1,5 @@
 public class MinMaxAve {
 
-    //Написать алгоритм MinMaxAve, который принимает массив чисел int[]  и 2 значения индексов.
-    //Алгоритм возвращает массив, который содержит минимальное значение, максимальное значение,  и среднее среди всех
-    //значений между 2-мя индексами.
-    //Test Data:
-    //({1, 2, 3, 4, 5, 6, 7, 8}, 2, 6) →  {3, 7, 5}
-
     public int[] MinMaxAveAlgorithm(int[] array, int index1, int index2) {
 
         if (array.length == 0 || index1 < 0 || index2 > (array.length - 1) || index1 > index2) {
