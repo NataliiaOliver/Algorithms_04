ba27d4bbaddaeca345641a20b414ef0367003beb
Nataliia Oliver <psvnatali@gmail.com>
2022-05-24 00:12:50 -0500

    code cleaned

--- src/main/java/OddEven.java
+++ src/main/java/OddEven.java
@@ -1,13 +1,5 @@
 public class OddEven {
 
-    //Создать алгоритм OddEven, который принимает на вход целое число, возвращает “Odd”,  если число нечетное, и “Even”,
-    //если число четное. Во всех остальных случаях результат будет “Undefined”.
-    //Test Data:
-    //-345 →  “Odd”
-    //0 →  “Even”
-    //222222 →  “Even”
-    //2147483647 + 1 →  “Undefined”
-
     public String oddEvenAlgorithm(long number) {
         String odd = "Odd";
         String even = "Even";
