95f2466cd812c3680e0cf3b164bffd0ca33d894a
Nataliia Oliver <psvnatali@gmail.com>
2022-05-24 00:21:10 -0500

    code cleaned

--- src/main/java/OddIndices.java
+++ src/main/java/OddIndices.java
@@ -1,10 +1,5 @@
 public class OddIndices {
 
-    //Написать алгоритм OddIndices, который принимает массив чисел,  и возвращает массив значений нечетных индексов
-    //Test Data:
-    //Input = {-45, 590, 234, 985, 12, 68}
-    //Expected Result =  {590, 985, 68}
-
     public int[] OddIndicesAlgorithm(int[] arrayNumbers) {
         if (arrayNumbers.length != 0) {
             int[] arrayOddIndices = new int[arrayNumbers.length / 2];
