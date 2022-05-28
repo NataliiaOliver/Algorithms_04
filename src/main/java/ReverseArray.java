24571f1c255dbf22c4014a5a697b1e76d64d3b72
Nataliia Oliver <psvnatali@gmail.com>
2022-05-27 22:33:49 -0500

    code cleaned

--- src/main/java/ReverseArray.java
+++ src/main/java/ReverseArray.java
@@ -1,9 +1,5 @@
 public class ReverseArray {
 
-    //Написать алгоритм ReverseArray, который принимает на вход массив целых чисел, и возвращает “перевернутый” массив.
-    //Test Data:
-    //{2, 7, 3, 10} → {10, 3, 7, 2}
-
     public int[] ReverseArrayAlgorithm(int[] array) {
 
         int[] result = new int[array.length];
