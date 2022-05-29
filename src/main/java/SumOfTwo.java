53a84fa6165f38bb7cd1d912480766db81dfed04
Nataliia Oliver <psvnatali@gmail.com>
2022-05-28 21:50:43 -0500

    code cleaned in file SumOfTwo

--- src/main/java/SumOfTwo.java
+++ src/main/java/SumOfTwo.java
@@ -1,10 +1,5 @@
 public class SumOfTwo {
 
-    //Написать алгоритм SumOfTwo, который принимает на вход массив целых чисел,  и число n.
-    //Алгоритм  возвращает пары элементов, которые в сумме дают число n.
-    //Test Data:
-    //({4, 3, 7, 12, 5, 2, 9, 4, 12}, 12)  → {{3, 9}, {7, 5}}
-
     public int[][] SumOfTwoAlgorithm(int[] array, int sum) {
         if (array.length > 0) {
 
