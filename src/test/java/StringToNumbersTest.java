f0e6beda78dbe7e4bf2653d7ceec55fcb0d55df4
Nataliia Oliver <psvnatali@gmail.com>
2022-06-03 14:12:35 -0500

    file StringToNumbersTest created, code added

--- src/test/java/StringToNumbersTest.java
+++ src/test/java/StringToNumbersTest.java
@@ -1,2 +1,17 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class StringToNumbersTest {
+
+    @Test
+    public void testStringToNumbersHappyPath() {
+
+        String str = "1, 2, 3, 4, 5";
+        int[] expectedResult = {1, 2, 3, 4, 5};
+
+        StringToNumbers sn = new StringToNumbers();
+        int[] actualResult = sn.stringToNumbersAlgorithm(str);
+
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
 }
